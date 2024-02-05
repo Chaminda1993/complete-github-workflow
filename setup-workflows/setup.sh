ebb52678d7f71f2f6843eaa349cbf84846695cc1
“Chaminda1993” <c771904768@gmail.com>
2024-02-05 23:40:54 +0530

    Local changes

--- setup-workflows/setup.sh
+++ setup-workflows/setup.sh
@@ -5,14 +5,14 @@ echo "Starting setup github actions workflow"
 mkdir -p ../.github/workflows/
 cp -f *.yml ../.github/workflows/
 cp -f *.toml ../
-cp -f *.sh ../
+cp -f install_Company_dependencies.sh ../
 echo "Setup completed"
 
 echo "Start pushing workflow to repository"
 cd ../
 git add .github/workflows/*.yml
 git add *.toml
-git add *.sh
+git add install_Company_dependencies.sh
 git commit -m "Seting up GitHub workflows"
 git push
 echo "Configuration successfully pushed to remote"
